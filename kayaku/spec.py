cb095436333e6b513ff70a8b14110be3db4834c5
BlueGlassBlock <blueglassblock@outlook.com>
2022-09-07 17:57:56 +0800

    feat: redesign source and path patterns

--- kayaku/spec.py
+++ kayaku/spec.py
@@ -1,114 +1,74 @@
 from __future__ import annotations
 
-from contextlib import suppress
+import enum
+import re
 from dataclasses import dataclass
-from typing import Literal
+from typing import cast
+
+from typing_extensions import TypedDict
+
+_LIG = r"[A-Za-z0-9_-]"
+_L_REP = rf"({_LIG}+\.)*"
+_R_REP = rf"(\.{_LIG}+)*"
+SOURCE_REGEX = re.compile(
+    rf"(?P<prefix>{_L_REP}){{(?P<sect_prefix>{_L_REP})\*\*(?P<sect_suffix>{_R_REP})}}(?P<suffix>{_R_REP})"
+)
 
 
 @dataclass
-class BaseSpec:
+class SectionSpec:
     prefix: list[str]
-    tag: str | None
+    suffix: list[str]
 
 
 @dataclass
-class PatternSpec(BaseSpec):
-    section: BaseSpec
+class SourceSpec:
+    prefix: list[str]
+    suffix: list[str]
+    section: SectionSpec
+
+
+class SourceRegexGroup(TypedDict):
+    prefix: str
+    sect_prefix: str
+    sect_suffix: str
+    suffix: str
+
+
+def parse_source(spec: str) -> SourceSpec:
+    if not (match_res := SOURCE_REGEX.fullmatch(spec)):
+        raise ValueError(f"{spec!r} doesn't match {SOURCE_REGEX.pattern!r}")
+    groups: SourceRegexGroup = cast(SourceRegexGroup, match_res.groupdict())
+    section: SectionSpec = SectionSpec(
+        groups["sect_prefix"].split(".")[:-1], groups["sect_suffix"].split(".")[1:]
+    )
+    return SourceSpec(
+        groups["prefix"].split(".")[:-1] + section.prefix,
+        section.suffix + groups["suffix"].split(".")[1:],
+        section,
+    )
+
+
+class PathFill(enum.Enum):
+    SINGLE = object()  # {} or {*}
+    EXTEND = object()  # {**}
 
 
 @dataclass
 class PathSpec:
-    path: list[str]
-    file_parts: int
-    section_suffix: list[str]
+    path: list[str | PathFill]
+    section: list[str | PathFill]
 
 
-def clear_buf(buf: list[str]) -> str:
-    res: str = "".join(buf)
-    buf.clear()
-    return res
-
-
-class Pattern:
-    def __init__(self, spec: str):
-        self.spec: str = spec
-        self.quote: Literal[None, '"', "'"] = None
-        self.tag_flag: bool = False
-        self.section_flag: Literal[None, True, False] = None
-        self.sect: BaseSpec = BaseSpec([], None)
-        self.res: PatternSpec = PatternSpec([], None, self.sect)
-        self.buf: list[str] = []
-
-    def parse_sep(self, index: int):
-        if not self.buf:
-            raise ValueError(f"Found empty specifier at {index}")
-        if self.res.tag is not None:
-            raise ValueError(f"Dot separator found after tag specifier at {index}")
-        if self.tag_flag:
-            self.res.tag = clear_buf(self.buf)
-            if self.section_flag:
-                self.sect.tag = self.res.tag
-        else:
-            self.res.prefix.append(clear_buf(self.buf))
-            if self.section_flag:
-                self.sect.prefix.append(self.res.prefix[-1])
-
-    def parse_tag_symbol(self, index: int):
-        if self.tag_flag:
-            raise ValueError(f"Duplicated tag symbol (:) found at {index}")
-        self.parse_sep(index)
-        self.tag_flag = True
-
-    def parse_r_bracket(self, it: "enumerate[str]", index: int):
-        if self.section_flag is True:
-            with suppress(StopIteration):
-                index, char = next(it)
-                if char not in (".", ":"):
-                    raise ValueError(
-                        f"Found right bracket not followed by separator at {index}"
-                    )
-                if char == ":":
-                    self.parse_tag_symbol(index)
-                    self.section_flag = False
-                    return
-            self.parse_sep(index)
-            self.section_flag = False
-        elif self.section_flag is False:
-            raise ValueError(f"Duplicated right curly bracket found at {index}")
-        else:
-            raise ValueError(f"Unmatched right curly bracket found at {index}")
-
-    def parse(self) -> PatternSpec:
-        it: "enumerate[str]" = enumerate(self.spec)
-        for index, char in it:
-            if self.quote is None:
-                if char == ".":
-                    self.parse_sep(index)
-                elif char == "{":
-                    if self.buf:
-                        raise ValueError(
-                            f"Found left bracket not following separator at {index}"
-                        )
-                    if self.section_flag is None:
-                        self.section_flag = True
-                    else:
-                        raise ValueError(f"Duplicated left bracket found at {index}")
-                elif char == "}":
-                    self.parse_r_bracket(it, index)
-                elif char == ":":
-                    self.parse_tag_symbol(index)
-                elif char in ('"', "'"):
-                    self.quote = char
-                else:
-                    self.buf.append(char)
-            elif self.quote == char:
-                self.quote = None
-            else:
-                self.buf.append(char)
-        if self.buf:
-            self.parse_sep(len(self.spec))
-        if self.res.prefix and self.res.prefix[-1] == "*":
-            self.res.prefix.pop()
-        if self.sect.prefix and self.sect.prefix[-1] == "*":
-            self.sect.prefix.pop()
-        return self.res
+def parse_path(spec: str) -> PathSpec:
+    replacer = {"{*}": PathFill.SINGLE, "{}": PathFill.SINGLE, "{**}": PathFill.EXTEND}
+    location, section = spec.split(":", 1)
+    if ":" in section:
+        raise ValueError(f"Spec {spec!r} contains multiple ':'")
+    path_parts: list[str | PathFill] = [replacer.get(l, l) for l in location.split("/")]
+    section_parts: list[str | PathFill] = [
+        replacer.get(l, l) for l in section.split(".")
+    ]
+    if path_parts.count(PathFill.EXTEND) + section_parts.count(PathFill.EXTEND) > 1:
+        raise ValueError(f"""Found more than one "extend" part ({{**}}) in {spec}""")
+    return PathSpec(path_parts, section_parts)
--- tests/spec.py
+++ tests/spec.py
@@ -1,62 +1,30 @@
 import pytest
 
-from kayaku.spec import BaseSpec, Pattern, PatternSpec
+from kayaku.spec import (
+    PathFill,
+    PathSpec,
+    SectionSpec,
+    SourceSpec,
+    parse_path,
+    parse_source,
+)
 
 
 def test_spec_parse():
-    assert Pattern("graia.ariadne").parse() == PatternSpec(
-        ["graia", "ariadne"], None, BaseSpec([], None)
+    assert parse_source("{module.**}.secrets") == SourceSpec(
+        ["module"], ["secrets"], SectionSpec(["module"], [])
     )
 
-    assert Pattern("{graia.ariadne}").parse() == PatternSpec(
-        ["graia", "ariadne"], None, BaseSpec(["graia", "ariadne"], None)
-    )
-
-    assert Pattern("{graia.ariadne:loc}").parse() == PatternSpec(
-        ["graia", "ariadne"], "loc", BaseSpec(["graia", "ariadne"], "loc")
-    )
-
-    assert Pattern("graia.{ariadne:loc}").parse() == PatternSpec(
-        ["graia", "ariadne"], "loc", BaseSpec(["ariadne"], "loc")
-    )
-
-    assert Pattern("graia.{ariadne}:loc").parse() == PatternSpec(
-        ["graia", "ariadne"], "loc", BaseSpec(["ariadne"], None)
-    )
-
-    assert Pattern(
-        """graia.ariadne.{"sect.iops":"mutable.tag"}"""
-    ).parse() == PatternSpec(
-        ["graia", "ariadne", "sect.iops"],
-        "mutable.tag",
-        BaseSpec(["sect.iops"], "mutable.tag"),
-    )
-
-    assert Pattern(
-        """graia.ariadne.{"sect.iops".pl:"mutable.tag"}"""
-    ).parse() == PatternSpec(
-        ["graia", "ariadne", "sect.iops", "pl"],
-        "mutable.tag",
-        BaseSpec(["sect.iops", "pl"], "mutable.tag"),
-    )
-
-    assert Pattern("graia.{ariadne.*}:loc").parse() == PatternSpec(
-        ["graia", "ariadne"], "loc", BaseSpec(["ariadne"], None)
+    assert parse_source("graia.{**}") == SourceSpec(["graia"], [], SectionSpec([], []))
+    assert parse_path("./config/modules/{}:config.{**}") == PathSpec(
+        [".", "config", "modules", PathFill.SINGLE], ["config", PathFill.EXTEND]
     )
 
 
 def test_parse_spec_err():
-    for spec in [
-        "a..b",
-        "a:b:c",
-        "a:b.c",
-        "a.{b}.{c}",
-        "a.{{b}",
-        "a.{c}}",
-        "a.a{bc",
-        "a.abc}",
-        "a.{b}.c}",
-    ]:
-        with pytest.raises(ValueError):
-            print(spec)
-            Pattern(spec).parse()
+    with pytest.raises(ValueError):
+        parse_source("*")
+    with pytest.raises(ValueError):
+        parse_path("{**}:{**}")
+    with pytest.raises(ValueError):
+        parse_path("a::b")
