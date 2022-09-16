c1aec9b7afe37a74f2ff384a3cbb9dc7f6f1e841
BlueGlassBlock <blueglassblock@outlook.com>
2022-09-16 18:46:06 +0800

    support array like update

--- kayaku/toml_utils/modify.py
+++ kayaku/toml_utils/modify.py
@@ -2,12 +2,56 @@ from __future__ import annotations
 
 from typing import Any
 
+from tomlkit.api import array, table
 from tomlkit.container import Container
-from tomlkit.items import AbstractTable, AoT, Array, InlineTable, Item, Trivia, item
+from tomlkit.items import (
+    AbstractTable,
+    AoT,
+    Array,
+    Comment,
+    InlineTable,
+    Item,
+    Null,
+    Trivia,
+    Whitespace,
+    item,
+)
 
 
-def update_array_like(item: AoT | Array, data: list) -> None:
-    ...
+def update_array_like(container: AoT | Array, data: list) -> None:
+    if container.unwrap() == data:
+        return
+    if isinstance(container, AoT):
+        new_body = []
+        if not all(isinstance(i, dict) for i in data):
+            raise TypeError(f"{data} is inappropriate for array of tables!")
+        for i, v in enumerate(data):
+            if container[i].unwrap() == v:
+                new_body.append(container[i])
+            else:
+                new_body.append(table(v))
+    else:
+        comments = []
+        for body in container._value:
+            if body.comment:
+                comments.append(Whitespace("\n"))
+                if isinstance(body.value, Null):
+                    comments.append(body.comment)
+                else:
+                    comments.append(
+                        Comment(
+                            Trivia(
+                                comment=f"# {body.value} {body.comment.trivia.comment}"
+                            )
+                        )
+                    )
+        arr = Array(
+            comments,
+            container.trivia,
+            bool(comments),
+        )
+        [arr.append(v) for v in data]
+        container.__dict__.update(arr.__dict__)
 
 
 def update_from_model(origin: Any, data: dict) -> None:
