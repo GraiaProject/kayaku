5d025a1408256da4546fc9471b1c5079bade6e38
BlueGlassBlock <blueglassblock@outlook.com>
2022-09-24 13:36:12 +0800

    support reload

--- kayaku/model.py
+++ kayaku/model.py
@@ -1,8 +1,8 @@
-import inspect
 from typing import Tuple, Type, TypeVar, Union, cast
 
 from loguru import logger
 from pydantic import BaseConfig, BaseModel, Extra
+from pydantic.utils import generate_model_signature
 
 from .doc_parse import store_field_description
 from .pretty import Prettifier
@@ -11,15 +11,39 @@ from .utils import update
 
 class ConfigModel(BaseModel):
     def __init_subclass__(cls, domain: str) -> None:
+        from .domain import _reg, domain_map, file_map
+
         domain_tup: Tuple[str, ...] = tuple(domain.split("."))
         if not all(domain_tup):
             raise ValueError(f"{domain!r} contains empty segment!")
-        from .domain import _reg, domain_map
 
         if domain_tup in domain_map:
-            raise NameError(
-                f"{domain!r} is already occupied by {domain_map[domain_tup]!r}"
-            )
+            other = domain_map[domain_tup]
+            if (
+                cls.__module__ == other.__module__
+                and cls.__qualname__ == other.__qualname__
+            ):
+                if other.__signature__ != (
+                    sig := generate_model_signature(
+                        cls.__init__, cls.__fields__, cls.__config__
+                    )
+                ):
+                    logger.warning(f"{cls} changed signature!")
+                    logger.warning(f"From: {other.__signature__}")
+                    logger.warning(f"To  : {sig}")
+                    logger.warning(
+                        "This change will not reflect in your schema or comments until next initialization!"
+                    )
+                domain_map[domain_tup] = cls
+                _reg.model_map.pop(other)
+                fmt_path = _reg.model_path.pop(other)
+                _reg.model_path[cls] = fmt_path
+                file_map[fmt_path.path][tuple(fmt_path.section)].remove(other)
+                file_map[fmt_path.path][tuple(fmt_path.section)].append(cls)
+                create(cls, flush=True)
+                return
+
+            raise NameError(f"{domain!r} is already occupied by {other!r}")
         domain_map[domain_tup] = cls
         if _reg.initialized:
             raise RuntimeError(
